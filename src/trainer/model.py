4cbb51fa0486a99d337dd519569d0155f45a63ba
Marshal Hayes <mdhayes.07@gmail.com>
2017-10-07 19:17:59 -0500

    adding model.py

--- src/trainer/model.py
+++ src/trainer/model.py
@@ -1,3 +1,10 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import multiprocessing
+
 import tensorflow as tf
 
 CSV_COLUMNS = ['position', 'result']

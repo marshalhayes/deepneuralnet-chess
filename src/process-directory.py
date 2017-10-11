5b3c5c5d761d807836baeb3617cbe2eb929712d2
Marshal Hayes <mdhayes.07@gmail.com>
2017-10-11 18:23:28 -0500

    row vectorization start

--- src/process-directory.py
+++ src/process-directory.py
@@ -1,6 +1,19 @@
 import re
 import glob
 
+# returns row vector corresponding to row fen string
+def vectorize_stripped_fen(fen_row_string):
+    row_vector = []
+    row_elements = list(fen_row_string)
+    for elem in row_elements:
+        if elem.isdigit():
+            for num in range(elem):
+                row_vector.append(0) # if the elem is a number, set that number of preceeding elements to empty squares (0)
+        else:
+            row_vector.append(elem) # if the elem is a piece (letter)
+
+    return row_vector
+
 regex = r'(\{(.*?)\}\ ((1\-0)|(0\-1)|(1\/2\-1\/2)))'
 outputfile = open('processed-output-fen_and_result.csv','wb')
 outputfile.write("position, color_to_move, result")
@@ -18,17 +31,7 @@ for filename in glob.glob('*.txt'):
                 matchcount += 1
                 fen, result = match.group(0)[2:].split('}')[:2]
                 position, whosmove = fen.split(' ')[:2]
-                outputfile.write(position[1:].replace('"', '') + "," + whosmove + "," + result.strip() + "\r\n")
-                print(position[1:].replace('"','') + "," + whosmove + "," + result.strip() + "\r\n")
-
-# returns row vector corresponding to fen parameter
-def vectorize_stripped_fen(fen):
-    rows = fen.split('/')
-    for row in rows:
-        expanded_row = []
-        character_list = list(row)
-        for character in character_list:
-            if character.isdigit():
-                for i in range(int(character)):
-                    expanded_row[i] = 0 # for the character amount, set that many squares to empty
-            else:
+                # outputfile.write(position[1:].replace('"', '') + "," + whosmove + "," + result.strip() + "\r\n")
+                # print(position[1:].replace('"','') + "," + whosmove + "," + result.strip() + "\r\n")
+                outputfile.write(",".join(vectorize_stripped_fen(position[1:].replace('"',''))) + "," + whosmove + "," + "\r\n")
+                print(str(match) + " matches found");

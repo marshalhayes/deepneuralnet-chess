b12146bf1885de4abaa8b85ec3f9bb368f941104
Marshal Hayes <mdhayes.07@gmail.com>
2017-10-11 17:33:17 -0500

    changing reading description

--- src/process-directory.py
+++ src/process-directory.py
@@ -1,5 +1,4 @@
 import re
-import csv
 import glob
 
 regex = r'(\{(.*?)\}\ ((1\-0)|(0\-1)|(1\/2\-1\/2)))'
@@ -9,7 +8,7 @@ outputfile.write("position, color_to_move, result")
 matchcount = 0
 for filename in glob.glob('*.txt'):
     print('######################################')
-    print(filename + " starting to be read")
+    print("Reading " + filename + " ... ")
     print('######################################')
     with open(filename) as f:
         print("Going through each line of " + filename)
@@ -21,3 +20,15 @@ for filename in glob.glob('*.txt'):
                 position, whosmove = fen.split(' ')[:2]
                 outputfile.write(position[1:].replace('"', '') + "," + whosmove + "," + result.strip() + "\r\n")
                 print(position[1:].replace('"','') + "," + whosmove + "," + result.strip() + "\r\n")
+
+# returns row vector corresponding to fen parameter
+def vectorize_stripped_fen(fen):
+    rows = fen.split('/')
+    for row in rows:
+        expanded_row = []
+        character_list = list(row)
+        for character in character_list:
+            if character.isdigit():
+                for i in range(int(character)):
+                    expanded_row[i] = 0 # for the character amount, set that many squares to empty
+            else:

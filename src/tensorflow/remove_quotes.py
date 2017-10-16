fc326acb4e6d0141b3ad03f1384428aed50b7ec5
Marshal Hayes <mdhayes.07@gmail.com>
2017-10-16 16:26:30 -0500

    starting gcloud setup

--- src/tensorflow/remove_quotes.py
+++
@@ -1,5 +0,0 @@
-outputfile = open("quotes-removed-test.csv", "w")
-with open("test-data.csv", "r") as f:
-    for line in f:
-        x = line.replace('"','')
-        outputfile.write(x)
---
+++ src/tensorflow/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages
+from setuptools import setup
+
+_name_ = 'deepneuralnet-chess'
+_version_ = '0.1'
+_install_requires_ = ['tensorflow>=1.3.0']
+_packages_ = find_packages()
+_include_package_data_ = True

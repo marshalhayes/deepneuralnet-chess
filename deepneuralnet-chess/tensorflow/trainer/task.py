6c10b69ac2742d12691b8cc206a54c95ae30553e
Marshal Hayes <mdhayes.07@gmail.com>
2017-11-12 00:08:02 -0600

    fixing col names AGAIN :(

--- deepneuralnet-chess/tensorflow/trainer/task.py
+++ deepneuralnet-chess/tensorflow/trainer/task.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import pandas as pd
 
-COLS_HEADERS = "a8,b8,c8,d8,e8,f8,g8,h8,a7,b7,c7,d7,e7,f7,g7,h7,a6,b6,c6,d6,e6,f6,g6,h6,a5,b5,c5,d5,e5,f5,g5,h5,a4,b4,c4,d4,e4,f4,g4,h4,a3,b3,c3,d3,e3,f3,g3,h3,a2,b2,c2,d2,e2,f2,g2,h2,a1,b1,c1,d1,e1,f1,g1,h1,whos_move,fen,result"
+COLS_HEADERS = "a1,b1,c1,d1,e1,f1,g1,h1,a2,b2,c2,d2,e2,f2,g2,h2,a3,b3,c3,d3,e3,f3,g3,h3,a4,b4,c4,d4,e4,f4,g4,h4,a5,b5,c5,d5,e5,f5,g5,h5,a6,b6,c6,d6,e6,f6,g6,h6,a7,b7,c7,d7,e7,f7,g7,h7,a8,b8,c8,d8,e8,f8,g8,h8,whos_move,fen,result"
 CSV_COLUMNS = COLS_HEADERS.split(',')
 
 # ----------------------------------------------------------------------------------------

829a1dba6d77633b6a75d69284e42b7a72302c1b
Marshal Hayes <mdhayes.07@gmail.com>
2017-11-14 16:17:19 -0600

    comment change

--- deepneuralnet-chess/tensorflow/trainer/model.py
+++ deepneuralnet-chess/tensorflow/trainer/model.py
@@ -10,7 +10,7 @@ import multiprocessing
 import tensorflow as tf
 
 
-# Define the format of your input data including unused columns
+# Define the format of the input data including unused columns
 COLS_HEADERS = "a1,b1,c1,d1,e1,f1,g1,h1,a2,b2,c2,d2,e2,f2,g2,h2,a3,b3,c3,d3,e3,f3,g3,h3,a4,b4,c4,d4,e4,f4,g4,h4,a5,b5,c5,d5,e5,f5,g5,h5,a6,b6,c6,d6,e6,f6,g6,h6,a7,b7,c7,d7,e7,f7,g7,h7,a8,b8,c8,d8,e8,f8,g8,h8,whos_move,fen,result"
 CSV_COLUMNS = COLS_HEADERS.split(',')
 
